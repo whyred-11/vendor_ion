2dd6452f1d8a5a03a91153c6cb1199d87ac07758
LuK1337 <priv.luk@gmail.com>
2019-10-24 02:33:34 +0200

    common: Bring back Android Beam
    * While this feature is deprecated all code
      for it is still alive ^.^
    
    Change-Id: I36fac374e43ceddab1438255654bb541937041ff

--- config/common.mk
+++ config/common.mk
@@ -73,6 +73,10 @@ $(foreach f,$(wildcard vendor/ion/prebuilt/common/etc/init/*.rc),\
 PRODUCT_COPY_FILES += \
     vendor/ion/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
 
+# Enable Android Beam on all targets
+PRODUCT_COPY_FILES += \
+    vendor/ion/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
