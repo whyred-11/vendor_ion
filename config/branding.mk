faf504ccac5a3159b6c520ff1a03124e2bb2cdd5
AnkitGourav <Ankitgourav035@gmail.com>
2020-01-26 02:26:29 +0530

    ion-2.3.b
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -5,9 +5,9 @@ ION_RELEASE_TYPE ?= Preview
 ION_PLATFORM_VERSION := 10.0
 
 ifeq ($(ION_RELEASE_TYPE), Release)
-    IONIZER := 2.3.a
-else
     IONIZER := 2.3.b
+else
+    IONIZER := 2.3.c
 endif
 
 TARGET_PRODUCT_SHORT := $(subst ion_,,$(ION_BUILD))
