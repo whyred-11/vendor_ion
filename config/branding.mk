137df76e14568c41699f684e1563a767eddc29cc
AnkitGourav <Ankitgourav035@gmail.com>
2020-06-04 22:17:04 +0530

    ion-2.7.a
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -2,7 +2,7 @@
 ION_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 ION_BUILD_TYPE ?= UNOFFICIAL
 ION_RELEASE_TYPE ?= Preview
-IONIZER := 2.6.b
+IONIZER := 2.7.a
 
 TARGET_PRODUCT_SHORT := $(subst ion_,,$(ION_BUILD))
 
