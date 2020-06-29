e3499d58bda7d66ba78e0cf9a9cc80bcf2415df7
Alexander Koskovich <zvnexus@outlook.com>
2020-06-28 22:24:53 -0400

    soong: Add TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS.
    Change-Id: Ibb4202eae237cc9f167707c45dd86a313d95d7be
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -61,6 +61,9 @@ type Product_variables struct {
 		Cflags []string
 		Whole_static_libs []string
 	}
+	Target_uses_prebuilt_dynamic_partitions struct {
+		Cflags []string
+	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
@@ -104,4 +107,5 @@ type ProductVariables struct {
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 	Needs_netd_direct_connect_rule  *bool `json:",omitempty"`
+	Target_uses_prebuilt_dynamic_partitions  *bool `json:",omitempty"`
 }
